a3e00f25b7b68829d268c324b7a946860ad58826
reginabecker <thinaregina@gmail.com>
2023-08-01 18:40:31 -0300

    Configurar para tela inicializar maximizada

--- src/main/java/mk/ukim/finki/seleniumtesting/BasePage.java
+++ src/main/java/mk/ukim/finki/seleniumtesting/BasePage.java
@@ -1,7 +1,10 @@
 package mk.ukim.finki.seleniumtesting;
 
+import com.sun.java.swing.plaf.windows.resources.windows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ResourceBundle;
 
 public class BasePage {
     public final WebDriverWait wait;
@@ -10,5 +13,6 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, 10);
+        driver.manage().window().maximize();
     }
 }
